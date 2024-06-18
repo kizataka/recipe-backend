85e1417850408b083e69f4594c96b5bd60a10597
kizataka <chongxiongmu@gmail.com>
2024-06-18 19:21:22 +0900

    factory_botでダミーデータを定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+   name { Faker::Name.name }
+   email { Faker::Internet.unique.email }
+   password { '12345678' }
+   password_confirmation { '12345678' } 
+  end
+end
