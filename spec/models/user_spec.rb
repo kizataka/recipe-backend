94168bbdc2bd3ab0887925f47837777be2f49639
kizataka <chongxiongmu@gmail.com>
2024-06-18 19:44:09 +0900

    factory_botを用いたコードに書き換え

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,54 +3,37 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "ユーザーのバリデーションに関するテスト" do
     it "名前、メール、パスワードがある場合は有効" do
-      user = User.new(
-        name: "kizataka",
-        email: "kizataka@example.com",
-        password_digest: "kizataka-password"
-      )
+      user = FactoryBot.build(:user)
       expect(user).to be_valid
     end
 
     it "名前がない場合は無効" do
-      user = User.new(name: nil)
+      user = FactoryBot.build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
 
     it "メールがない場合は無効" do
-      user = User.new(email: nil)
+      user = FactoryBot.build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it "メールアドレスが重複している場合は無効" do
-      User.create(
-        name: "kizataka",
-        email: "kizataka@example.com",
-        password_digest: "kizataka-password",
-      )
-      user = User.new(
-        name: "takataka",
-        email: "kizataka@example.com",
-        password_digest: "kizataka-password"
-      )
+      FactoryBot.create(:user, email: "kizataka@example.com")
+      user = FactoryBot.build(:user, email: "kizataka@example.com")
       user.valid?
       expect(user.errors[:email]).to include("has already been taken")
     end
 
     it "パスワードがない場合は無効" do
-      user = User.new(password_digest: nil)
+      user = FactoryBot.build(:user, password_digest: nil)
       user.valid?
       expect(user.errors[:password_digest]).to include("can't be blank")
     end
 
     it "パスワード（確認用）とパスワードが一致しない場合は無効" do
-      user = User.new(
-        name: "kizataka",
-        email: "kizataka@example.com",
-        password: "kizataka-password",
-        password_confirmation: "takataka-password"
-      )
+      user = FactoryBot.build(:user, password_digest: "password", password_confirmation: "different")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
