6d0d729dee00713c95486634705faec13448a178
kizataka <chongxiongmu@gmail.com>
2024-06-22 22:25:32 +0900

    リクエストスペックの追加

--- spec/requests/api/accounts_spec.rb
+++ spec/requests/api/accounts_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Accounts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/account" do
+    let(:user_params) do
+      { name: "kizataka", email: "kizataka@example.com", password: "12345678", password_confirmation: "12345678"}
+    end
+    it "ユーザー作成ができること" do
+      expect do
+        post "/api/account", params: user_params, as: :json
+        expect(response).to have_http_status(200)
+        expect(response.parsed_body).to match({
+          "id" => be_present,
+          "name" => "kizataka",
+          "email" => "kizataka@example.com",
+        })
+      end.to change { User.count }.by(1)
+    end
+
+    let(:invalid_user_params) do
+      { name: "kizataka", email: "kizataka@email.com", password: "12345678", password_confirmation: "1234"}
+    end
+    it "ユーザーの作成に失敗すること" do
+      post "/api/account", params: invalid_user_params, as: :json
+      expect(response).to have_http_status(422)
+      expect(response.parsed_body["error"]).to be_present
+    end
   end
 end
