1d29ae9aa7b1b7472538de8ada8776dda885a24e
kizataka <chongxiongmu@gmail.com>
2024-06-23 15:37:44 +0900

    リクエストスペックの追加

--- spec/requests/api/dishes_spec.rb
+++ spec/requests/api/dishes_spec.rb
@@ -1,7 +1,131 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Dishes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/dishes" do
+    let!(:dishes) { create_list(:dish, 21) }
+    it "レシピの一覧が取得できること" do
+      get "/api/dishes"
+      expect(response).to have_http_status(200)
+      expect(response.parsed_body["dishes"].size).to eq(10)
+      expect(response.parsed_body["dishes"][0].keys).to match_array(%w[id title description image created_at updated_at user])
+      expect(response.parsed_body["paging"]).to match(
+      "total_pages" => 3,
+      "total_count" => 21,
+      "current_page" => 1
+      )
+    end
+  end
+
+  describe "POST /api/dishes" do
+    let(:user) { create(:user) }
+    let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
+    let(:headers) { { Authorization: "Bearer #{token}" } }
+    let(:dish_params) { { title: "hoge", description: "hoge", image: "hoge" } }
+    context "ログイン済みの場合" do
+      it "レシピが作成できること" do
+        expect do
+          post("/api/dishes", params: dish_params, headers:, as: :json)
+          expect(response).to have_http_status(200)
+          expect(response.parsed_body).to match(
+            "id" => be_present,
+            "title" => "hoge",
+            "description" => "hoge",
+            "image" => "hoge",
+            "created_at" => be_present,
+            "updated_at" => be_present,
+            "user" => include("id" => user.id)
+          )
+        end.to change(Dish, :count).by(1)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "401エラーになること" do
+        post "/api/dishes", params: dish_params, as: :json
+        expect(response).to have_http_status(401)
+        json = response.parsed_body
+        expect(json["error"]).to match("messages" => be_present)
+      end
+    end
+  end
+
+  describe "GET /api/dishes/:id" do
+    let(:dish) { create(:dish) }
+    it "レシピの詳細を取得できること" do
+      get "/api/dishes/#{dish.id}"
+      expect(response).to have_http_status(200)
+      expect(response.parsed_body).to match(
+        "id" => dish.id,
+        "title" => dish.title,
+        "description" => dish.description,
+        "image" => dish.image,
+        "created_at" => be_present,
+        "updated_at" => be_present,
+        "user" => include("id" => dish.user.id)
+      )
+    end
+  end
+
+  describe "PATCH /api/dishes/:id" do
+    let(:user) { create(:user) }
+    let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
+    let(:headers) { { Authorization: "Bearer #{token}" } }
+    let(:dish) { create(:dish, user:) }
+    let(:dish_params) { { title: "hoge", description: "hoge", image: "hoge" } }
+    context "ログイン済みの場合" do
+      it "レシピの更新ができること" do
+        patch("/api/dishes/#{dish.id}", params: dish_params, headers:, as: :json)
+        expect(response).to have_http_status(200)
+        expect(response.parsed_body).to match(
+          "id" => be_present,
+          "title" => "hoge",
+          "description" => "hoge",
+          "image" => "hoge",
+          "created_at" => be_present,
+          "updated_at" => be_present,
+          "user" => include("id" => user.id)
+        )
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "401エラーになること" do
+        patch "/api/dishes/#{dish.id}", params: dish_params, as: :json
+        expect(response).to have_http_status(401)
+        json = response.parsed_body
+        expect(json["error"]).to match("messages" => be_present)
+      end
+    end
+  end
+
+  describe "DELETE /api/dishes/:id" do
+    let(:user) { create(:user) }
+    let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
+    let(:headers) { { Authorization: "Bearer #{token}" } }
+    let(:dish) { create(:dish, user:) }
+    context "ログイン済みの場合" do
+      it "レシピが削除できること" do
+        delete("/api/dishes/#{dish.id}", headers:)
+        expect(response).to have_http_status(200)
+        expect(response.parsed_body).to match(
+          "id" => be_present,
+          "title" => dish.title,
+          "description" => dish.description,
+          "image" => dish.image,
+          "created_at" => be_present,
+          "updated_at" => be_present,
+          "user" => include("id" => user.id)
+        )
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "401エラーになること" do
+        delete "/api/dishes/#{dish.id}"
+        expect(response).to have_http_status(401)
+        json = response.parsed_body
+        expect(json["error"]).to match("messages" => be_present)
+      end
+    end
   end
 end
--- spec/requests/api/sessions_spec.rb
+++ spec/requests/api/sessions_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/session" do
+    let!(:user) { create(:user) }  # 全てのテストが始まる前に作成されている必要がある
+    context "認証情報が正しい場合" do
+      let(:session_params) { { email: user.email, password: "12345678" } }
+      it "ログインに成功すること" do
+        post "/api/session", params: session_params,  as: :json
+        expect(response).to have_http_status(200)
+        expect(response.parsed_body["token"]).to be_a(String)
+      end
+    end
+
+    context "認証情報に誤りがある場合" do
+      let(:invalid_session_params) { { session: { email: user.email, password: "1234" } } }
+      it "ログインに失敗すること" do
+        post "/api/session", params: invalid_session_params
+        expect(response).to have_http_status(401)
+        expect(response.parsed_body["error"]).to be_present
+      end
+    end
   end
 end
