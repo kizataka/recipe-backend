fdf85f0d710d782271b7091f70bc80e43f14de26
kizataka <chongxiongmu@gmail.com>
2024-06-23 12:30:53 +0900

    titleの文字列を20文字以内に制限

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :dish do
-    title { Faker::Food.dish }
+    title { Faker::Lorem.characters(number: 20) }  # 20文字以内のランダムな文字列
     description { Faker::Food.description }
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/dummy.png'), 'image/png') }
     user
